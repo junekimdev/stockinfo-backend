2a1736f3d8149fccc9bb1ff1f7aeeb245ab895b7
June Kim <godlyjune@gmail.com>
2024-06-10 16:51:57 +0900

    Add headers: HOST, USER_AGENT in req

--- src/services/companies/provider.rs
+++ src/services/companies/provider.rs
@@ -4,8 +4,10 @@ use crate::utils::{db, error::Error, settings::Settings, Result};
 #[tracing::instrument(err)]
 pub async fn build_company_db() -> Result<()> {
     let web_client = reqwest::Client::new();
-    let url = Settings::instance().urls.kr_company.clone();
     let key = Settings::instance().keys.data_go_kr.clone();
+    let url = Settings::instance().urls.kr_company.clone();
+    let req_url = reqwest::Url::parse(&url).unwrap();
+    let host = req_url.host_str().unwrap();
     let format = time::macros::format_description!("[year][month][day]");
     let mut base_date = time::OffsetDateTime::now_utc()
         .date()
@@ -14,8 +16,10 @@ pub async fn build_company_db() -> Result<()> {
 
     // Get all codes
     let mut res = web_client
-        .get(&url)
-        .header(reqwest::header::ACCEPT_CHARSET, "utf-8")
+        .get(req_url.clone())
+        .header(reqwest::header::HOST, host)
+        .header(reqwest::header::USER_AGENT, "StockinfoRuntime/1.0.0")
+        .header(reqwest::header::ACCEPT, "application/json;charset=UTF-8")
         .query(&[
             ("serviceKey", key.as_str()),
             ("resultType", "json"),
@@ -32,8 +36,10 @@ pub async fn build_company_db() -> Result<()> {
         base_date = base_date.previous_day().unwrap();
 
         res = web_client
-            .get(&url)
-            .header(reqwest::header::ACCEPT_CHARSET, "utf-8")
+            .get(req_url.clone())
+            .header(reqwest::header::HOST, host)
+            .header(reqwest::header::USER_AGENT, "StockinfoRuntime/1.0.0")
+            .header(reqwest::header::ACCEPT, "application/json;charset=UTF-8")
             .query(&[
                 ("serviceKey", key.as_str()),
                 ("resultType", "json"),
