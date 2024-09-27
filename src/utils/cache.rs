7e64882d89f496c64c21461f2f94269facb41c94
June Kim <godlyjune@gmail.com>
2024-09-27 18:13:15 +0900

    Use name, version, and api URL as a key for cache
    - new version will use new cache
    - key scheme: `[name]_[version]:[URL]`

--- src/utils/cache.rs
+++ src/utils/cache.rs
@@ -23,8 +23,9 @@ pub async fn set<V>(k: &str, v: &V) -> Result<()>
 where
     V: ToRedisArgs + std::marker::Sync + std::fmt::Debug,
 {
+    let key = get_key_prefixed(k);
     let mut conn = pool().get().await?;
-    conn.set_ex::<&str, &V, ()>(k, v, 86400).await?; // 86400 = 24*60*60 : 1 day in sec
+    conn.set_ex::<&str, &V, ()>(&key, v, 86400).await?; // 86400 = 24*60*60 : 1 day in sec
     Ok(())
 }
 
@@ -34,14 +35,22 @@ pub async fn set_with_timer<V>(k: &str, v: &V, seconds: u64) -> Result<()>
 where
     V: ToRedisArgs + std::marker::Sync + std::fmt::Debug,
 {
+    let key = get_key_prefixed(k);
     let mut conn = pool().get().await?;
-    conn.set_ex::<&str, &V, ()>(k, v, seconds).await?;
+    conn.set_ex::<&str, &V, ()>(&key, v, seconds).await?;
     Ok(())
 }
 
 #[tracing::instrument(err)]
 pub async fn get<T: FromRedisValue>(k: &str) -> Result<T> {
+    let key = get_key_prefixed(k);
     let mut conn = pool().get().await?;
-    let res = conn.get::<&str, T>(k).await?;
+    let res = conn.get::<&str, T>(&key).await?;
     Ok(res)
+}
+
+fn get_key_prefixed(k: &str) -> String {
+    let app_name = std::env::var("CARGO_PKG_NAME").unwrap_or("app".to_string());
+    let app_version = std::env::var("CARGO_PKG_VERSION").unwrap_or("0.0.0".to_string());
+    app_name + "_" + &app_version + ":" + k
 }
